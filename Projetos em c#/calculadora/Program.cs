ceaabfed5c5401febf2e6c6cfd5ba9a7e027b0b2
Eduardo Raupp <eduardovicente73@gmail.com]>
2024-04-08 17:59:28 -0300

    app da calculadora finalizado

--- Projetos em c#/calculadora/Program.cs
+++ Projetos em c#/calculadora/Program.cs
@@ -6,24 +6,52 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
-            Subtracao();
+            Menu();
         }
-        static void Soma ()
+
+        static void Menu()
         {
-        Console.Clear();
+            Console.Clear();
+            Console.WriteLine("O que deseja calcular? ");
+            Console.WriteLine("1 - soma");
+            Console.WriteLine("2 - subtração");
+            Console.WriteLine("3 - divisão");
+            Console.WriteLine("4 - multiplicação");
+            Console.WriteLine("5 - Sair");
+            Console.WriteLine("--------------");
+            Console.WriteLine("selecione uma opção;");
+            short res = short.Parse(Console.ReadLine());
 
-        Console.WriteLine("digite o primeiro valor: ");
-        float v1 = float.Parse(Console.ReadLine()); //utilizei float.parse para converter a string em numero
-        Console.WriteLine("digite o segundo valor");
-        float v2 = float.Parse(Console.ReadLine());
-        float resultado = v1 + v2;
-        Console.WriteLine(""); //Só para pular uma linha
-        Console.WriteLine("o resultado da soma dos 2 numeros é " + resultado);
-        Console.ReadKey();
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
 
-}
 
-        static void Subtracao(){
+        }
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine()); //utilizei float.parse para converter a string em numero
+            Console.WriteLine("digite o segundo valor");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 + v2;
+            Console.WriteLine(""); //Só para pular uma linha
+            Console.WriteLine("o resultado da soma dos 2 numeros é " + resultado);
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        static void Subtracao()
+        {
             Console.Clear();
             Console.WriteLine("digite o primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
@@ -32,10 +60,47 @@ namespace calculadora
             float v2 = float.Parse(Console.ReadLine());
             Console.WriteLine("");
 
-        float resultado = v1 - v2;
+            float resultado = v1 - v2;
             Console.WriteLine($"o resultado da subtração é {resultado}");
 
-        Console.ReadKey();
+            Console.ReadKey();
+            Menu();
         }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"o resultado da divisão é {resultado}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicao()
+        {
+            Console.Clear();
+            Console.WriteLine("digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"o resultado da multiplicação é {resultado}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+
     }
 }
--- Projetos em c#/calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ Projetos em c#/calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29652a35c1c04d23fadaa4b6a223509d4fbc26ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4fa94b46683bc67cc62ad551c7c1ef7b4c350d")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
